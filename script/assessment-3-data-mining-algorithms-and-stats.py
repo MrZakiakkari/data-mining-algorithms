61d499ab2d9ea69e551c9584cb6c1b210e4d14d4
M.Zaki Al Akkari <56824972+MrZakiakkari@users.noreply.github.com>
2022-05-16 22:51:50 +0100

    update

--- script/assessment-3-data-mining-algorithms-and-stats.py
+++ script/assessment-3-data-mining-algorithms-and-stats.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# <!--
+# import data_analytics.github as github
+# print(github.create_jupyter_notebook_header("mrzakiakkari", "data-mining-algorithms", "assessment-3-data-mining-algorithms-and-stats.ipynb", "master"))
+# -->
+# <table style="margin: auto;"><tr><td><a href="https://mybinder.org/v2/gh/mrzakiakkari/data-mining-algorithms/master?filepath=assessment-3-data-mining-algorithms-and-stats.ipynb" target="_parent"><img src="https://mybinder.org/badge_logo.svg" alt="Open In Binder"/></a></td><td>online editors</td><td><a href="https://colab.research.google.com/github/mrzakiakkari/data-mining-algorithms/blob/master/assessment-3-data-mining-algorithms-and-stats.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a></td></tr></table>
+
 # # Assessment 3 Data Mining Algorithms
 # 
 # Where appropriate add two cells below the question.  One a code cell to hold any code you used, and the second a markdown cell to hold the explanation.  
 # You are to submit your notebook file, which should be labelled yourname_assign3
 # Note the questions are allocated different weightings, these will be scaled to 100%.
 
+from pandas import DataFrame
+import seaborn
 import numpy as np
 import pandas as pd  # To read data
 from sklearn.linear_model import LinearRegression
 import seaborn as sns  #To visualise
+from data_analytics.graphs import display_correlation_matrix_pyramid_heatmap
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 
 
-diamonds = sns.load_dataset("diamonds")
+diamonds_dataframe: DataFrame = seaborn.load_dataset("diamonds")
 
-diamonds.columns
+diamonds_dataframe.columns
 
 
 # ### Diamonds meta data
@@ -55,7 +64,63 @@ diamonds.columns
 # #### 1. In the diamonds data set identify quantitative variables that have  linear relationships
 # ####  <div style="text-align: right"> (5 marks) </div><br>
 
-# put the code you used in here
+# ## Exploratory Data Analysis
+
+diamonds_dataframe.head()
+
+
+diamonds_dataframe.info()
+diamonds_dataframe.shape
+diamonds_dataframe.describe()
+
+
+# Points to notice:
+# 
+# Min value of "x", "y", "z" are zero this indicates that there are faulty values in data that represents dimensionless or 2-dimensional diamonds. So we need to filter out those as it clearly faulty data points.
+# 
+# 
+
+invalid_diamonds_dataframe = diamonds_dataframe[(diamonds_dataframe["x"]==0) |
+                   (diamonds_dataframe["y"]==0) |
+                   (diamonds_dataframe["z"]==0)]
+
+invalid_diamonds_dataframe.sample()
+
+diamonds_dataframe.drop(invalid_diamonds_dataframe.index, inplace=True)
+del invalid_diamonds_dataframe
+diamonds_dataframe.shape
+
+
+# #### Interpretation of correlation coefficient
+# 
+# Correlation size | Interpretation
+# -|-
+# &plusmn; 1.00 to 1.00 | Perfect correlation
+# &plusmn; 0.90 to 0.99 | Very high correlation
+# &plusmn; 0.70 to 0.90 | High correlation
+# &plusmn; 0.50 to 0.70 | Moderate correlation
+# &plusmn; 0.30 to 0.50 | Low correlation
+# &plusmn; 0.00 to 0.30 | Negligible correlation
+# 
+# <p class="Caption">Correlation Interpretation Table</p>
+# 
+# This table suggests the interpretation of correlation size at different absolute values. These cut-offs are arbitrary and should be used judiciously while interpreting the dataset.
+# 
+
+display_correlation_matrix_pyramid_heatmap(diamonds_dataframe[["carat", "depth", "table", "price", "x", "y", "z"]].corr());
+
+
+# <p class="Caption">Correlation Matrix Heat Map Pyramid</p>
+# 
+# #### Correlations suggesting investigation
+# Consider correlation Threshold &GreaterEqual; 0.85.  
+# 
+# Feature one  | Feature two |  Correlation size
+# :-|:-|-
+# Caret | Price | 0.92
+# Caret | X | 0.98
+
+
 
 
 # #answer your question and give explanation in here
@@ -64,17 +129,54 @@ diamonds.columns
 
 # #### 2 Create a linear regression model predicting the price of a diamond using <b><u>ONE independent variable</b></u>.<br>
 # #### <div style="text-align: right"> (15 marks) </div><br>
+# 
+# Price works with X, Y, Z and caret
+
+X = diamonds_dataframe[["x","y"]]#we will use RM - average number of rooms per dwelling
+y = diamonds_dataframe[["price"]]#we want to predict Y - Median value of owner-occupied 
+
+
+#Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+#Create the regressor: reg
+reg = LinearRegression()
+
+#Fit the regressor to the training data
+reg.fit(X_train, y_train)
+
+# Predict on the test data: y_pred
+y_pred = reg.predict(X_test)
+
+# Compute and print RMSE between our predicted MEDV and actual MEDV
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+
+
+
+print("score:",reg.score(X, y)) #Return the coefficient of determination of the prediction.
+print("coef_:",reg.coef_)#Estimated coefficients for the linear regression problem. 
+print("intercept_:",reg.intercept_)
+print("rmse: ",rmse)
+
+
 # #### Test the different <i>quantitative</i> columns in order to identify which independent variable has the most predictive power for price.<br>
 # #### In a markdown cell, provide justification as to why you chose the variable<br>
 # Remember to use a reasonable split on the data to create test and train subsets 
 
-
-
-
-
-
-
+# caret  
+# score: 0.8493313275232197  
+# coef_: [[7761.92989215]]    
+# intercept_: [-2261.72208262]  
+# rmse:  1582.3550379033259  
+# x  
+# score: 0.7871769599303438  
+# coef_: [[3165.68767825]]  
+# intercept_: [-14211.73813022]  
+# rmse:  1831.417285579254  
 # 
+
+
+
 
 
 
